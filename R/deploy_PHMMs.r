8f255cbd0c5dbaa5b48fcb83dfa3a1747a868c92
Cam Nugent <nugentc@uoguelph.ca>
2019-07-22 12:24:19 -0400

    newer set_frame function added

--- R/deploy_PHMMs.r
+++ R/deploy_PHMMs.r
@@ -14,15 +14,69 @@ individual_AAbin = function(aa_string){
 	return(as.AAbin(strsplit(as.character(aa_string),"")))
 }
 
+#TODO - think about combining leading_ins and ins_front_trim into a single function
+#at present they're highly similar
+
+#a check for a large number of leading inserted bases,
+#if this is the case, TRUE is returned and the PHMM
+#should be run a second time on the truncated data. 
+leading_ins = function(seq_path){
+	#iterate along the sequence, if we hit 5 insertions
+	#before 5 matches then there is a problem
+	matches = 0
+	ins = 0
+	for(x in seq_path){
+		if (x == 1){
+			matches = matches + 1
+		} else if (x == 2){
+			ins = ins + 1
+		}
+
+		if (matches == 5){
+			return(FALSE)
+		}
+		if (ins == 5){
+			return(TRUE)
+		}
+	}
+}
+
+# check sequence for an early large string of deletions, if it exists then
+# return the starting index by which to slice the path and the string
+ins_front_trim = function(path_out, search_scope = 15){
+	
+	if(sum(path_out[1:search_scope] == 2)>2){
+		run_pos = min(which(path_out == 2))
+		for(i in run_pos:length(path_out)){
+			if(path_out[i] != 2){
+				return(i)
+			}
+		}
+	}
+
+	return(FALSE)
+}
+
+
 #take an input sequence and get it into the reading frame for proper translation based on the
 #nucleotide PHMM output. This is more conservative, just what is required to get the aaPHMM
 #running, as opposed to making corrections to the nucleotides first
 set_frame = function(org_seq , path_out){
 	org_seq_vec = strsplit(tolower(org_seq), split='')[[1]]
 
+	adj_for_dels = ins_front_trim(path_out)
+
+	#If there are a large number (>2) of deletes in the first 15bp
+	#then adjust the sequence so the end of the delete run is the starting point
+	if(adj_for_dels != FALSE){
+		org_seq_vec = org_seq_vec[adj_for_dels:length(org_seq_vec)]
+		path_out = path_out[adj_for_dels:length(path_out)]
+	}
+
 	front = c()
 	org_seq_start = 1
 	org_seq_end = length(org_seq_vec)
+
 
 	for( i in 1:length(path_out) ){
 		#0 = D
@@ -76,26 +130,5 @@ set_frame = function(org_seq , path_out){
 }
 
 
-#a check for a large number of leading inserted bases,
-#if this is the case, TRUE is returned and the PHMM
-#should be run a second time on the truncated data. 
-leading_ins = function(seq_path){
-	#iterate along the sequence, if we hit 5 insertions
-	#before 5 matches then there is a problem
-	matches = 0
-	ins = 0
-	for(x in seq_path){
-		if (x == 1){
-			matches = matches + 1
-		} else if (x == 2){
-			ins = ins + 1
-		}
 
-		if (matches == 5){
-			return(FALSE)
-		}
-		if (ins == 5){
-			return(TRUE)
-		}
-	}
-}
+
