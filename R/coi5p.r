8929e9bf962e239188ff12b8eb8fc3e2c706b49b
Cam Nugent <nugentc@uoguelph.ca>
2019-07-23 12:13:24 -0400

    reading on how to set up functions in pkg

--- R/coi5p.r
+++ R/coi5p.r
@@ -8,6 +8,37 @@
 
 #use: package::function() syntax for external functions so as to make it explicicit that that is needed
 
+#NOTE FOR GETTING PHMMS and TRANS TABLES in:
+# aphid has a folder /data with .RData (binary?) objects within that store the data, presumably I'll need ot do this.
+
+
+#' This is how to document in a way that can be accessed by ?coi
+#' Add roxygen comments to your .R files.
+#' Run devtools::document() (or press Ctrl/Cmd + Shift + D in RStudio) to convert roxygen comments to .Rd files. (devtools::document() calls roxygen2::roxygenise() to do the hard work.)
+#' Preview documentation with ?.
+#' Rinse and repeat until the documentation looks the way you want.
+#' It will generate a man/foo.Rd file, which should not be modified by hand
+#' There is shorthand for arguments, examples etc. see the book, most common for functions:
+#' @param
+#' @examples
+#' @return
+foo = function(x){
+
+}
+
+#data structure - use an s3 or s4 class to store the data associated with the functions in a standardized way
+#may be a good idea to associate all of the functions with the class, have the PHMMs be stored within the class?
+#
+
+#^ read these two sections of adv-r and decide which is more appropriate
+# s3 seems very flexible and recommended
+# s4 has documented slots so may be better to use that to store the data in its different forms
+
+
+
+
+
+
 coi = function(str){
   #take a string as input and turin it into the decided upon data structure
     #store original str in $raw
