2893505722ac6e05e1e5acfeb96124698773019b
Cam Nugent <nugentc@uoguelph.ca>
2019-08-14 15:54:38 -0400

    new frame logic working and tests passed

--- tests/testthat/test_shortSeq.r
+++ tests/testthat/test_shortSeq.r
@@ -3,10 +3,10 @@
 test_that("A short sequence is framed and translated properly", {
 
   sequence = 'tatgctagggaccgcagttagtgtgattattcgtgctgagttaggacagccaggatcacttattgggaacgatcaaatttacaatacaattgtgactgctcatgcctttattataattttcttcatggtgatacctatcataatcggaggattcggtaattgactggtaccggtaatactaggagcaccagatatagctttccctcgtatgaacaacataagattttgattactccctccttccttaacccttcttataatcgggatactaacagaaagaggggcaggaacaggatgaactgtataccctcctctctcaagaaatatccctcactcaggagctagagtagacctaacaattttttcactacatttagctggagccaggtcacttcttggggctattaatttcatcacaacaattattaatatacgagcagctagaatatctcttgatcgaattcctttatttg'
-  sequence_framed = '--------------------------------tatgctagggaccgcagttagtgtgattattcgtgctgagttaggacagccaggatcacttattgggaacgatcaaatttacaatacaattgtgactgctcatgcctttattataattttcttcatggtgatacctatcataatcggaggattcggtaattgactggtaccggtaatactaggagcaccagatatagctttccctcgtatgaacaacataagattttgattactccctccttccttaacccttcttataatcgggatactaacagaaagaggggcaggaacaggatgaactgtataccctcctctctcaagaaatatccctcactcaggagctagagtagacctaacaattttttcactacatttagctggagccaggtcacttcttggggctattaatttcatcacaacaattattaatatacgagcagctagaatatctcttgatcgaattcctttatttg'
+  sequence_framed = '--------------------------------tatgctagggaccgcagttagtgtgattattcgtgctgagttaggacagccaggatcacttattgggaacgatcaaatttacaatacaattgtgactgctcatgcctttattataattttcttcatggtgatacctatcataatcggaggattcggtaattgactggtaccggtaatactaggagcaccagatatagctttccctcgtatgaacaacataagattttgattactccctccttccttaacccttcttataatcgggatactaacagaaagaggggcaggaacaggatgaactgtataccctcctctctcaagaaatatccctcactcaggagctagagtagacctaacaattttttcactacatttagctggagccaggtcacttcttggggctattaatttcatcacaacaattattaatatacgagcagctagaatatctcttgatcgaattccttta'
 
-  sequence_AAcensored = "-----------MLGTAVSVIIRAELGQPGSLIGNDQIYNTIVTAHAFI?IFFMV?PI?IGGFGNWLVPV?LGAPD?AFPRMNN??FWLLPPSLTLL?IG?LTE?GAGTGWTVYPPLS?NIPHSGA?VDLTIFSLHLAGA?SLLGAINFITTIIN?RAA??SLDRIPLF-"
-  sequence_AA5 = "-----------MLGTAVSVIIRAELGQPGSLIGNDQIYNTIVTAHAFIMIFFMVMPIMIGGFGNWLVPVMLGAPDMAFPRMNNMSFWLLPPSLTLLMIGMLTESGAGTGWTVYPPLSSNIPHSGASVDLTIFSLHLAGASSLLGAINFITTIINMRAASMSLDRIPLF"
+  sequence_AAcensored = "-----------MLGTAVSVIIRAELGQPGSLIGNDQIYNTIVTAHAFI?IFFMV?PI?IGGFGNWLVPV?LGAPD?AFPRMNN??FWLLPPSLTLL?IG?LTE?GAGTGWTVYPPLS?NIPHSGA?VDLTIFSLHLAGA?SLLGAINFITTIIN?RAA??SLDRIPL"
+  sequence_AA5 = "-----------MLGTAVSVIIRAELGQPGSLIGNDQIYNTIVTAHAFIMIFFMVMPIMIGGFGNWLVPVMLGAPDMAFPRMNNMSFWLLPPSLTLLMIGMLTESGAGTGWTVYPPLSSNIPHSGASVDLTIFSLHLAGASSLLGAINFITTIINMRAASMSLDRIPL"
 
   dat = coi5p(sequence )
   expect_equal(dat$raw, sequence)
@@ -35,63 +35,55 @@ test_that("A short sequence is framed and translated properly", {
   sequence_2 = 'attatacctcctatttggggcctgagcaggaatagtagggacagccctaagcatcctaattcgagcagaacttggacagccaggcgctctattaggtgacgaccaaatttataacgtagtcgttacagcccacgcattcgtcataattttcttcatagtaataccaataataatcggcgggttcggtaactgattagtcccactaataattggagcccccgacatagcattcccacgaataaacaatataagcttctgactcctgcccccatcattccttctacttttagcctcatctatagtagaagcaggggccggaacaggatgaactgtttacccaccactagccggcaatttagcacacgcaggagcatccgtagacctaaccattttttccctacacctagcaggagtttcatcaattttaggcgctatcaatttcattactaccattatcaatataaaaccgccagccatatcacaatatcaaacccccctatttgtgtgatcagtccttattaccgccgtactacttctcctctccctcccagtcttagccgcaggcattacaatactcttaaccgaccgaaatctaaatactacattcttcgacccagccggaggcggtgaccccattttataccaacatcta'
   #trimmed version of this, two missing from front
            # "acattatacctcctatttggggcctgagcaggaatagtagggacagccctaagcatcctaattcgagcagaacttggacagccaggcgctctattaggtgacgaccaaatttataacgtagtcgttacagcccacgcattcgtcataattttcttcatagtaataccaataataatcggcgggttcggtaactgattagtcccactaataattggagcccccgacatagcattcccacgaataaacaatataagcttctgactcctgcccccatcattccttctacttttagcctcatctatagtagaagcaggggccggaacaggatgaactgtttacccaccactagccggcaatttagcacacgcaggagcatccgtagacctaaccattttttccctacacctagcaggagtttcatcaattttaggcgctatcaatttcattactaccattatcaatataaaaccgccagccatatcacaatatcaaacccccctatttgtgtgatcagtccttattaccgccgtactacttctcctctccctcccagtcttagccgcaggcattacaatactcttaaccgaccgaaatctaaatactacattcttcgacccagccggaggcggtgaccccattttataccaacatcta"
-  sequence2_framed = ''
+  sequence2_framed = '---ttatacctcctatttggggcctgagcaggaatagtagggacagccctaagcatcctaattcgagcagaacttggacagccaggcgctctattaggtgacgaccaaatttataacgtagtcgttacagcccacgcattcgtcataattttcttcatagtaataccaataataatcggcgggttcggtaactgattagtcccactaataattggagcccccgacatagcattcccacgaataaacaatataagcttctgactcctgcccccatcattccttctacttttagcctcatctatagtagaagcaggggccggaacaggatgaactgtttacccaccactagccggcaatttagcacacgcaggagcatccgtagacctaaccattttttccctacacctagcaggagtttcatcaattttaggcgctatcaatttcattactaccattatcaatataaaaccgccagccatatcacaatatcaaacccccctatttgtgtgatcagtccttattaccgccgtactacttctcctctccctcccagtcttagccgcaggcattacaatactcttaaccgaccgaaatctaaatactacattcttcgacccagccggaggcggtgaccccattttataccaacatcta'
 
-  sequence2_AAcensored = ''
-  sequence2_AA5 = ''
+  sequence2_AAcensored = '-LYLLFGAWAG?VGTALSILIRAELGQPGALLGDDQIYNVVVTAHAFV?IFF?V?P??IGGFGNWLVPL?IGAPD?AFPR?NN?SFWLLPPSFLLLLASS?VEAGAGTGWTVYPPLAGNLAHAGASVDLTIFSLHLAGVSSILGAINFITTIIN??PPA?SQYQTPLFVWSVLITAVLLLLSLPVLAAGIT?LLTDRNLNTTFFDPAGGGDPILYQHL'
+  sequence2_AA5 = '-LYLLFGAWAGMVGTALSILIRAELGQPGALLGDDQIYNVVVTAHAFVMIFFMVMPMMIGGFGNWLVPLMIGAPDMAFPRMNNMSFWLLPPSFLLLLASSMVEAGAGTGWTVYPPLAGNLAHAGASVDLTIFSLHLAGVSSILGAINFITTIINMKPPAMSQYQTPLFVWSVLITAVLLLLSLPVLAAGITMLLTDRNLNTTFFDPAGGGDPILYQHL'
 
-
-  dat2 = coi5p(sequence )
-  expect_equal(dat2$raw, sequence)
+  dat2 = coi5p(sequence_2 )
+  expect_equal(dat2$raw, sequence_2)
   expect_identical(dat2$name, character(0))
 
   dat2 = frame(dat2)
-  expect_equal(dat2$framed, sequence_framed)
+  expect_equal(dat2$framed, sequence2_framed)
 
   dat2 = translate(dat2)
-  expect_equal(dat2$aaSeq, sequence_AAcensored)
-
-  dat2 = indel_check(dat2)
-  expect_equal(dat2$indel_likely, TRUE)
-  expect_equal(dat2$stop_codons, FALSE)
-
-  dat2 = translate(dat2, trans_table = 2)
-  expect_equal(dat2$aaSeq, sequence_AA5)
+  expect_equal(dat2$aaSeq, sequence2_AAcensored)
 
   dat2 = indel_check(dat2)
   expect_equal(dat2$indel_likely, FALSE)
   expect_equal(dat2$stop_codons, FALSE)
 
+  dat2 = translate(dat2, trans_table = 2)
+  expect_equal(dat2$aaSeq, sequence2_AA5)
 
-
-
-
+  dat2 = indel_check(dat2)
+  expect_equal(dat2$indel_likely, FALSE)
+  expect_equal(dat2$stop_codons, FALSE)
 
   sequence_3 = 'ggcgctcttctgggggatgaccaaatctataacgtgatcgtcacagcccatgccttcgttatgattttctttatagtcatgccaattataatcgggggctttggaaactgattaattcccctaataatcggagcccctgatatggcattccctcgaataaataacataagcttctgactccttcctccatcctttctcctcctcctgtcttcatcaggagttgaagccggcgcgggtactggatgaacagtatacccccctctagccggcaacctcgcccacgcaggagcctctgttgatttaactatcttctcccttcatttagctggaatctcctcaattttaggagccattaattttattacgaccattattaacataaaacctccagccatctctcagtaccaaaccccccttttcgtttgagccgtgctagttactgctgtccttctattactttccctccccgtcctggca'
   #trimmed version of this, 81 missing from front, 93 missing from back.
   #"ctctatttagtatttggtgcctgagccgggatagtaggcaccgccctgagtctactgattcgggcggaactaagccagccgggcgctcttctgggggatgaccaaatctataacgtgatcgtcacagcccatgccttcgttatgattttctttatagtcatgccaattataatcgggggctttggaaactgattaattcccctaataatcggagcccctgatatggcattccctcgaataaataacataagcttctgactccttcctccatcctttctcctcctcctgtcttcatcaggagttgaagccggcgcgggtactggatgaacagtatacccccctctagccggcaacctcgcccacgcaggagcctctgttgatttaactatcttctcccttcatttagctggaatctcctcaattttaggagccattaattttattacgaccattattaacataaaacctccagccatctctcagtaccaaaccccccttttcgtttgagccgtgctagttactgctgtccttctattactttccctccccgtcctggcagcaggcattactatgttacttacagaccgaaatctaaacaccactttctttgacccggcaggcgggggagatccaattttataccaacacctc"
-  sequence3_framed = ''
+  sequence3_framed = '---------------------------------------------------------------------------------------gctcttctgggggatgaccaaatctataacgtgatcgtcacagcccatgccttcgttatgattttctttatagtcatgccaattataatcgggggctttggaaactgattaattcccctaataatcggagcccctgatatggcattccctcgaataaataacataagcttctgactccttcctccatcctttctcctcctcctgtcttcatcaggagttgaagccggcgcgggtactggatgaacagtatacccccctctagccggcaacctcgcccacgcaggagcctctgttgatttaactatcttctcccttcatttagctggaatctcctcaattttaggagccattaattttattacgaccattattaacataaaacctccagccatctctcagtaccaaaccccccttttcgtttgagccgtgctagttactgctgtccttctattactttccctccccgtcctggc'
+  sequence3_AAcensored = '-----------------------------ALLGDDQIYNVIVTAHAFVMIFF?VMPI?IGGFGNWLIPL?IGAPDMAFPR?NN?SFWLLPPSFLLLLSSSGVEAGAGTGWTVYPPLAGNLAHAGASVDLTIFSLHLAGISSILGAINFITTIIN??PPAISQYQTPLFVWAVLVTAVLLLLSLPVL-'
+  sequence3_AA5 = '-----------------------------ALLGDDQIYNVIVTAHAFVMIFFMVMPIMIGGFGNWLIPLMIGAPDMAFPRMNNMSFWLLPPSFLLLLSSSGVEAGAGTGWTVYPPLAGNLAHAGASVDLTIFSLHLAGISSILGAINFITTIINMKPPAISQYQTPLFVWAVLVTAVLLLLSLPVL'
 
-  sequence3_AAcensored = ''
-  sequence3_AA5 = ''
-
-
-  dat3 = coi5p(sequence )
-  expect_equal(dat3$raw, sequence)
+  dat3 = coi5p(sequence_3 )
+  expect_equal(dat3$raw, sequence_3)
   expect_identical(dat3$name, character(0))
 
   dat3 = frame(dat3)
-  expect_equal(dat3$framed, sequence_framed)
+  expect_equal(dat3$framed, sequence3_framed)
 
   dat3 = translate(dat3)
-  expect_equal(dat3$aaSeq, sequence_AAcensored)
+  expect_equal(dat3$aaSeq, sequence3_AAcensored)
 
   dat3 = indel_check(dat3)
-  expect_equal(dat3$indel_likely, TRUE)
+  expect_equal(dat3$indel_likely, FALSE)
   expect_equal(dat3$stop_codons, FALSE)
 
   dat3 = translate(dat3, trans_table = 2)
-  expect_equal(dat3$aaSeq, sequence_AA5)
+  expect_equal(dat3$aaSeq, sequence3_AA5)
 
   dat3 = indel_check(dat3)
   expect_equal(dat3$indel_likely, FALSE)
